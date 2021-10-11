0ce924fc51ba596888ac9650dbe8e4df2cbd44c5
zahon <zahongirabdulhamidov@gmail.com>
2021-10-11 22:57:37 +0500

    changed css & js

--- script.js
+++ script.js
@@ -2,25 +2,23 @@ const bars = document.querySelector('.fa-bars');
 const back = document.querySelector('.fa-times');
 const nav = document.querySelector('.nav');
 const navH1 = document.querySelectorAll('.nav h1');
+const circle = document.querySelector('.menu');
 const content = document.querySelector('.content');
 
 bars.addEventListener('click', () => {
 	nav.classList.add('active');
-	back.classList.add('active');
 	navH1.forEach(e => {
 		e.classList.add('h1-active');
 	});
-	content.classList.add('rotate');
-	bars.classList.add('rotate');
-	// back.classList.remove('active');
+	circle.classList.add('active');
+	content.classList.add('active');
 });
 
 back.addEventListener('click', () => {
 	nav.classList.remove('active');
-	back.classList.remove('active');
 	navH1.forEach(e => {
 		e.classList.remove('h1-active');
 	});
-	content.classList.remove('rotate');
-	bars.classList.remove('rotate');
+	circle.classList.remove('active');
+	content.classList.remove('active');
 });
